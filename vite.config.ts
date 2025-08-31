54cd87a772554c6cfcb1c719d1e3afa562fc89fc
junweiq <aooaqoob@gmail.com>
2025-08-31 20:24:28 +0800

    新增多入口配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {createHtmlPlugin} from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin(createHtmlPluginConfig())
+  ],
 })
+
+function createHtmlPluginConfig () {
+  type UserOptions = typeof createHtmlPlugin extends (options?: infer O) => any ? O : never;
+  const config: UserOptions = {
+    minify: true,
+    pages: [
+      {
+        entry: 'src/main.tsx',
+        filename: 'index.html',
+        template: 'index.html',
+        injectOptions: {
+          data: {
+            title: 'index',
+          },
+        },
+      },
+      {
+        entry: 'src/main.tsx',
+        filename: 'other.html',
+        template: 'other.html',
+        injectOptions: {
+          data: {
+            title: 'other page',
+          },
+        },
+      },
+    ],
+  }
+
+  return config
+}
