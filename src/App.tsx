a9fc4e4606d48a81df52d31bec404402a1ac26f6
junweiq <aooaqoob@gmail.com>
2025-09-08 20:55:35 +0800

    新增換膚

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import '../public/testvar.css'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState<string | null>(null)
+
+  function updateTheme () {
+    const themeList = [null, 'light', 'dark']
+    let idx = themeList.indexOf(theme) + 1
+    if (idx >= themeList.length) {
+      idx = 0
+    }
+    setTheme(themeList[idx])
+  }
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = theme ?? ''
+  }, [theme]);
 
   return (
     <>
@@ -16,10 +31,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1><span style={{color: 'var(--color-a01)'}}>Vite</span> + <span style={{color: 'var(--color-a02)'}}>React</span></h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+        <div style={{display: 'inline-block', width: 20}} />
+        <button onClick={updateTheme}>
+          theme is {String(theme)}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
